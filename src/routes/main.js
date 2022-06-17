7e02f32a6bbc6a78fa9f4fc3900f06d7d7fbf9e1
Thiago Sales Calabria <thiago.sales@apis3.com>
2022-06-17 11:50:46 -0300

    página interna produto

--- src/routes/main.js
+++ src/routes/main.js
@@ -2,11 +2,15 @@ var express = require('express');
 var router = express.Router();
 const mainController= require('../controllers/mainController')
 const productsController= require('../controllers/productsController')
+const productController= require('../controllers/productController')
 
 /* GET home page. */
 router.get('/',mainController.index)
 
 /* GET products page. */
 router.get('/produtos', productsController.index)
+
+/* GET internal product page. */
+router.get('/produto/:slug', productController.index)
 
 module.exports = router;
