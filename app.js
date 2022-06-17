ee6c7d7ca6663b56de850bd3ca6a117741a5dc52
Thiago Sales Calabria <thiago.sales@apis3.com>
2022-06-17 13:32:03 -0300

    Inicio do login

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var createError = require('http-errors');
 var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var mainRouter = require('./src/routes/main');
 var usersRouter = require('./src/routes/users');
 
@@ -16,9 +17,11 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret:'abcde', resave: true, saveUninitialized: true}));
 
 app.use('/', mainRouter);
 app.use('/users', usersRouter);
