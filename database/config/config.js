e11ed1cb32b4b1f0fe27ff0b84528733093a52ae
Thiago Sales Calabria <thiago.sales@apis3.com>
2022-06-27 21:12:00 -0300

    Ajustes config database

--- database/config/config.js
+++ database/config/config.js
@@ -1,9 +1,23 @@
-const config = {
-    username: 'root',
-    password: '',
-    database: 'supermercado',
-    host: 'localhost',
-    dialect: 'mysql'
+{
+  "development": {
+    "username": "root",
+    "password": "",
+    "database": "supermercado",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
 }
-
-module.exports = config;
