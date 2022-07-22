e59e076c19d322738bc2217542c5b9f6deb725dc
Thiago Sales Calabria <thiago.sales@canaltech.com>
2022-07-22 20:32:46 -0300

    Trazendo produto do banco

--- src/controllers/admProdutoController.js
+++ src/controllers/admProdutoController.js
@@ -32,14 +32,44 @@ const admProdutoController = {
    },
 
    salvar:async (req,res)=>{
+
+
+      function slugify(str)
+      {
+         str = str.replace(/^\s+|\s+$/g, '');
+
+         // Make the string lowercase
+         str = str.toLowerCase();
+
+         // Remove accents, swap ñ for n, etc
+         var from = "ÁÄÂÀÃÅČÇĆĎÉĚËÈÊẼĔȆÍÌÎÏŇÑÓÖÒÔÕØŘŔŠŤÚŮÜÙÛÝŸŽáäâàãåčçćďéěëèêẽĕȇíìîïňñóöòôõøðřŕšťúůüùûýÿžþÞĐđßÆa·/_,:;";
+         var to   = "AAAAAACCCDEEEEEEEEIIIINNOOOOOORRSTUUUUUYYZaaaaaacccdeeeeeeeeiiiinnooooooorrstuuuuuyyzbBDdBAa------";
+         for (var i=0, l=from.length ; i<l ; i++) {
+            str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+         }
+
+         // Remove invalid chars
+         str = str.replace(/[^a-z0-9 -]/g, '') 
+         // Collapse whitespace and replace by -
+         .replace(/\s+/g, '-') 
+         // Collapse dashes
+         .replace(/-+/g, '-'); 
+
+         return str;
+      }
+
+
+      $slug = slugify(req.body.nome);
+
       try{
          
          const resultado= await db.Produto.create({
             nome:req.body.nome,
-            slug:req.body.slug,
+            slug: $slug,
             preco:req.body.preco,
             categoria:req.body.categoria,
-            descricao:req.body.descricao
+            descricao:req.body.descricao,
+            imagem: req.body.imagem ?? '/img/produtos/produto8.jpg',
          })
 
          console.log(resultado)
--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,6 +1,12 @@
 var product = { produto: "Sobrecoxa Sadia Bio 1 Kg", slug: "a", 
 preco: "12,95", image:"/img/produtos/produto1.jpg"};
 
+const product = await db.Produto.find({
+    where: {
+        slug: req.params
+    }
+})
+
 const productController={
 
     index:(req,res)=>{
